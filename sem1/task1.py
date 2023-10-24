2a59014cef596dde50a34a5b0db59104ec4d9a2b
ilya.devel <ilya.devel@yandex.ru>
2023-10-24 23:04:38 +0500

    first seminar completed

---
+++ sem1/task1.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "HELLO WORLD!"
+
+
+@app.route("/about/")
+def about():
+    return "ABOUT"
+
+
+@app.route("/contact/")
+def contact():
+    return "CONTACT"
+
+
+@app.route("/<int:num1>/<int:num2>/")
+def summarize(num1, num2):
+    return str(num1 + num2)
+
+
+@app.route("/get-length-row/<string:row>/")
+def get_length_row(row):
+    return str(len(row))
+
+
+@app.route("/get-html/")
+def get_html():
+    return """
+    <h1>HELLO!</h1>
+    """
+
+
+@app.route("/students/")
+def get_info_about_students():
+    students = [
+        {"name": "alex", "surname": "black", "age": 17, "rating": 4.5},
+        {"name": "alex", "surname": "black", "age": 17, "rating": 4.5},
+        {"name": "alex", "surname": "black", "age": 17, "rating": 4.5},
+        {"name": "alex", "surname": "black", "age": 17, "rating": 4.5},
+        {"name": "alex", "surname": "black", "age": 17, "rating": 4.5}
+    ]
+    return render_template("students.html", students=students)
+
+
+@app.route("/news/")
+def news():
+    news = [
+        {"title": "TITLE", "date": "24.10.2023", "description": "some newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome news"},
+        {"title": "TITLE", "date": "24.10.2023", "description": "some newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome news"},
+        {"title": "TITLE", "date": "24.10.2023", "description": "some newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome news"},
+        {"title": "TITLE", "date": "24.10.2023", "description": "some newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome news"},
+        {"title": "TITLE", "date": "24.10.2023", "description": "some newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome newssome news"},
+    ]
+    return render_template("news2.html", news=news)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
